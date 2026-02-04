ab16c2f9453a3ac2ee6557fa4777f30db5ec3cb2
Plush N <118629584+ggz2@users.noreply.github.com>
2026-02-04 12:22:32 -0800

    Remove init function from main.js
    Removed the init function and its associated logic.

--- main.js
+++ main.js
@@ -11,28 +11,3 @@ async function try_mirror(mirror) {
   window.base_url = mirror;
   eval(script);
 }
-
-async function init() {
-  if (window.location.hostname == "edpuzzle.hs.vc") {
-    alert(
-      "To use this, drag this button into your bookmarks bar. Then, run it when you're on an Edpuzzle assignment."
-    );
-    return;
-  }
-  if (document.dev_env) {
-    return try_mirror(document.dev_env);
-  }
-
-  for (let mirror of mirrors) {
-    try {
-      await try_mirror(mirror);
-      return;
-    } catch {}
-  }
-
-  alert(
-    "Error: Could not connect to any of the mirrors. Check that they're not blocked."
-  );
-}
-
-init();
